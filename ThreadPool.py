d9fb0fdcc02acd49deebdfe56490566978f93985
Yehuda (Igor) Itkin <ig.itkin@gmail.com>
2015-10-10 22:41:43 +0300

    Initial commit

---
+++ ThreadPool.py
@@ -0,0 +1,102 @@
+__author__ = 'Yehuda (Igor) Itkin'
+from multiprocessing import Queue, Process, Lock, Manager, Value
+import time, logging
+from random import randrange
+import timeit
+
+# Preventing DDOS. This number is arbitrary
+MAX_NUMBER_JOBS_IN_QUEUE = 1000
+
+
+def convert_to_full(func, *args, **kwargs):
+    def wrapped():
+        return func(*args, **kwargs)
+
+    return wrapped
+
+
+class UsersQueue(object):
+    def __init__(self, maxsize=0):
+        self.q = Queue(maxsize)
+        self.time = Value('d', 0)
+        self.tasks_submitted = Value('i', 0)
+        self.task_completed = Value('i', 0)
+        self.order_lock = Lock()
+        self.sched_lock = Lock()
+
+
+class Worker(Process):
+    """Thread executing tasks from a given tasks queue"""
+
+    def __init__(self, tasks, killer):
+        Process.__init__(self)
+        self.tasks = tasks
+        self.daemon = True
+        self.killer = killer
+        self.logger = logging.getLogger('Worker')
+        self.start()
+
+    def is_all_tasks_empty(self):
+        for task in self.tasks:
+            if not task.q.empty():
+                return False
+        return True
+
+    def run(self):
+        while not self.killer.value or not self.is_all_tasks_empty():
+
+            i = randrange(len(self.tasks))
+            self.tasks[i].order_lock.acquire()
+            # Critical section
+            if self.tasks[i].q.empty():
+                self.tasks[i].order_lock.release()
+                continue
+
+            func, args, kwargs = self.tasks[i].q.get()
+            wrapped_func = convert_to_full(func, *args, **kwargs)
+            try:
+                t = timeit.timeit(wrapped_func, number=1)
+                self.tasks[i].time.value += t
+                self.tasks[i].task_completed.value += 1
+            except Exception as e:
+                self.tasks[i].order_lock.release()
+                self.logger.warning(e)
+                continue
+
+            # end of critical section
+            self.tasks[i].order_lock.release()
+
+
+class ThreadPool:
+    """Pool of threads consuming tasks from a queue"""
+
+    def __init__(self, num_threads, num_users):
+        self.procs = []
+        self.tasks = []
+        self.num_users = num_users
+        self.die = Value('b', False)
+        self.tasks_submitted = 0
+        for _ in range(num_users):
+            self.tasks.append(UsersQueue(0))
+
+        for _ in range(num_threads):
+            p = Worker(self.tasks, self.die)
+            self.procs.append(p)
+
+    def kill(self):
+        self.die.value = True
+
+    def add_task(self, num, func, *args, **kargs):
+        """Add a task to the queue"""
+
+        """Because MAX_NUMBER_JOBS_IN_QUEUE is aproximate, I dont use lock here"""
+        if not self.die.value and self.tasks[num].tasks_submitted.value < MAX_NUMBER_JOBS_IN_QUEUE:
+            self.tasks[num].q.put((func, args, kargs))
+            self.tasks_submitted += 1
+            self.tasks[num].tasks_submitted.value += 1
+        else:
+            raise Exception('Unable to add task')
+
+    def wait_completion(self):
+        """Wait for completion of all the tasks in the queue"""
+        [p.join() for p in self.procs]
--- main.py
+++ main.py
@@ -1,1 +1,81 @@
 __author__ = 'Yehuda (Igor) Itkin'
+
+from multiprocessing import Queue, Process, Lock, Manager, Value
+import time, logging
+from random import randrange
+
+from ThreadPool import ThreadPool
+
+if __name__ == '__main__':
+
+    num_of_users = 2
+    num_of_process = num_of_users
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                        datefmt='%d/%m/%y %H:%M')
+
+    logger = logging.getLogger('main')
+    logger.setLevel(logging.INFO)
+
+
+    def wait_delay(d, task_num):
+        logger = logging.getLogger('running task %d' % task_num)
+        logger.info('sleeping for %d sec', d)
+        time.sleep(d)
+
+    def producer(pool, user, delay, lifetime=60.0):
+        logger = logging.getLogger('producer %d' % user)
+        start_time = time.time()
+        real_time_for_jobs = 0
+        task_num = 0
+        while time.time() - start_time < lifetime:
+            d = randrange(1, 10)  # sleep up to 10 sec
+            try:
+                logger.info('Adding task: sleep for %d secs (task %d)', d, task_num)
+                pool.add_task(user, wait_delay, d, task_num)
+                real_time_for_jobs += d
+            except Exception as e:
+                logging.warning(e)
+            task_num += 1
+            time.sleep(delay)
+        logger.info('user %s submitted jobs for %d secs', user, real_time_for_jobs)
+
+
+    # 1) Init a Thread pool with the desired number of threads and number of users
+    #    in the real world apps, the optimal number of threads = num_cores + 1
+    pool = ThreadPool(num_of_process, num_of_users)
+    producers = []
+
+    for user in range(num_of_users):
+        base_time = 10.0  # the slowest user
+        delay = base_time / (user + 1)
+        p = Process(target=producer, args=(pool, user, delay,))
+        producers.append(p)
+        p.start()
+
+    # 3) Wait for completion
+    [p.join() for p in producers]
+
+    pool.kill()
+    pool.wait_completion()
+
+    logger.info('Real times:')
+    real_time = list(enumerate([task.time.value for task in pool.tasks]))
+    logger.info(real_time)
+    task_completed = reduce(lambda res, task: res + task.task_completed.value, pool.tasks, 0)
+    logger.info('Completed_tasks: %d from %d', task_completed, pool.tasks_submitted)
+    logger.info('statistics for users: ')
+
+    def print_statistics(user, task):
+        logger.info('user %d:', user)
+        logger.info('\ttask_submitted: %d', task.tasks_submitted.value)
+        logger.info('\ttask_completed: %d', task.task_completed.value)
+        logger.info('\ttime of active work: %s', task.time.value)
+
+
+    [print_statistics(i, task) for i, task in enumerate(pool.tasks)]
+
+
+
+
+
